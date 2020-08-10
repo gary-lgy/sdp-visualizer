c6c55dbf928f8ee1017ba1d32be2e204d287f1e3
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-08-10 23:47:45 +0800

    Add fab to scroll to top and bottom

--- src/components/SDPInput.tsx
+++ src/components/SDPInput.tsx
@@ -1,12 +1,17 @@
 import {
   Box,
   Button,
+  Fab,
   makeStyles,
   TextField,
   Theme,
+  useScrollTrigger,
   useTheme,
+  Zoom,
 } from "@material-ui/core";
-import React from "react";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import React, { useRef } from "react";
 
 interface Props {
   onSubmit: () => void;
@@ -24,11 +29,27 @@ const useStyles = makeStyles((theme: Theme) => ({
       margin: theme.spacing(2),
     },
   },
+  fab: {
+    position: "fixed",
+    zIndex: theme.zIndex.modal,
+    right: theme.spacing(2),
+    bottom: theme.spacing(2),
+    "& > *": {
+      marginLeft: theme.spacing(1),
+    },
+  },
 }));
 
 export const SDPInput: React.FC<Props> = (props) => {
   const theme = useTheme();
   const classes = useStyles(theme);
+
+  const topRef = useRef<HTMLDivElement | null>(null);
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const shouldDisplayScrollFab = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 10,
+  });
 
   const handleTextValueChange = (
     event: React.ChangeEvent<HTMLTextAreaElement>
@@ -41,40 +62,65 @@ export const SDPInput: React.FC<Props> = (props) => {
     props.onSubmit();
   };
 
+  const handleScroll = (
+    ref: React.MutableRefObject<HTMLDivElement | null>
+  ) => () => {
+    ref.current?.scrollIntoView();
+  };
+
   return (
-    <form
-      noValidate
-      autoComplete="off"
-      onSubmit={handleSubmit}
-      className={classes.root}
-    >
-      <TextField
-        value={props.sdp}
-        onChange={handleTextValueChange}
-        variant="outlined"
-        multiline
-        autoFocus
-        fullWidth
-        placeholder="Paste the SDP here"
-      />
-      <Box className={classes.buttonGroup}>
-        <Button
-          color="secondary"
-          variant="contained"
-          disabled={props.sdp.length === 0}
-          onClick={() => props.setSDP("")}
-        >
-          Clear
-        </Button>
-        <Button
-          type="submit"
-          color="primary"
-          variant="contained"
-          disabled={props.sdp.length === 0}
-        >
-          Visualize
-        </Button>
+    <Box>
+      <div ref={topRef}></div>
+
+      <Box className={classes.fab}>
+        <Zoom in={shouldDisplayScrollFab}>
+          <Fab color="primary" onClick={handleScroll(topRef)}>
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </Zoom>
+        <Zoom in={shouldDisplayScrollFab}>
+          <Fab color="primary" onClick={handleScroll(bottomRef)}>
+            <KeyboardArrowDownIcon />
+          </Fab>
+        </Zoom>
       </Box>
-    </form>
+
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+        className={classes.root}
+      >
+        <TextField
+          value={props.sdp}
+          onChange={handleTextValueChange}
+          variant="outlined"
+          multiline
+          autoFocus
+          fullWidth
+          placeholder="Paste the SDP here"
+        />
+        <Box className={classes.buttonGroup}>
+          <Button
+            color="secondary"
+            variant="contained"
+            disabled={props.sdp.length === 0}
+            onClick={() => props.setSDP("")}
+          >
+            Clear
+          </Button>
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            disabled={props.sdp.length === 0}
+          >
+            Visualize
+          </Button>
+        </Box>
+      </form>
+
+      <div ref={bottomRef}></div>
+    </Box>
   );
 };
