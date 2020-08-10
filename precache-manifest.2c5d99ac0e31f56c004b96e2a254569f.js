5064978ad885746bebacdc2248208ea4ba177c91
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2020-08-11 01:14:49 +0800

    Updates

---
+++ precache-manifest.2c5d99ac0e31f56c004b96e2a254569f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68c5f1b0f2335411634920f981c483c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee42b62971a3addfddac",
+    "url": "/static/js/2.d2f7bb0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d2f7bb0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fb5d45683a8393776d",
+    "url": "/static/js/main.53f2b951.chunk.js"
+  },
+  {
+    "revision": "0047555b59d633084752",
+    "url": "/static/js/runtime-main.c1a946c1.js"
+  }
+]);
